405571b7ff786532cc22f3a3417b5bdb1a423d51
Andrey Vydra <69751912+vydramain@users.noreply.github.com>
2026-07-24 19:01:49 +0600

    feat: Implemented infinite disc_run with frame loop

--- pdk/include/pdk/de/rv_de.hpp
+++ pdk/include/pdk/de/rv_de.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #include "pdk/rv_pdko.hpp"
 
 namespace rv_3dmppc {
--- src/rv_pconsole/rv_pconsole.cpp
+++ src/rv_pconsole/rv_pconsole.cpp
@@ -1,5 +1,11 @@
 #include "rv_pconsole.hpp"
 
+#include <chrono>
+#include <cmath>
+
+#include "pdk/rv_err.hpp"
+#include "rv_infra/rv_log.hpp"
+
 rv_3dmppc::rv_ca* rv_3dmppc::rv_pconsole::ca() { return &ca_; }
 rv_3dmppc::rv_cd* rv_3dmppc::rv_pconsole::cd() { return &cd_; }
 rv_3dmppc::rv_cm* rv_3dmppc::rv_pconsole::cm() { return &cm_; }
@@ -9,6 +15,24 @@ rv_3dmppc::rv_cv* rv_3dmppc::rv_pconsole::cv() { return &cv_; }
 // rv_3dmppc::rv_de& rv_3dmppc::rv_pconsole::disc_load(const char* /*path*/) {};
 
 int64_t rv_3dmppc::rv_pconsole::disc_run(rv_3dmppc::rv_de& disc) {
-    disc.disc_initialize(*this);
+    int64_t dir = disc.disc_initialize(*this);
+    if (0 > dir) {
+        RV_LOG_ERR("pconsole",
+                   "Can't initialize mppcdisc in console. Please check mppcdisc consistency: {}",
+                   dir);
+        return rv_3dmppc::RV_ERR_INVAL;
+    }
+
+    std::chrono::duration<float> dt{};
+    std::chrono::time_point t_prev = std::chrono::steady_clock::now();
+    do {
+        std::chrono::time_point t_now = std::chrono::steady_clock::now();
+        dt = t_now - t_prev;
+        t_prev = t_now;
+
+        disc.frame_update(dt.count());
+        disc.frame_render();
+    } while (!disc.disc_release());
+
     return rv_3dmppc::RV_OK;
 }
