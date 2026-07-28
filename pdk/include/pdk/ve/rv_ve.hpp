ef9fd7f3f580b3087d196effd4e971eb52e10c7f
Andrey Vydra <69751912+vydramain@users.noreply.github.com>
2026-07-29 05:06:01 +0600

    chore: Rename version vocabulary to RV_MPPC

--- pdk/include/pdk/ve/rv_ve.hpp
+++ pdk/include/pdk/ve/rv_ve.hpp
@@ -6,23 +6,28 @@
 
 namespace rv_pdk {
 
-inline constexpr int RV_3DMPPC_VERSION_MAJOR = 0;
-inline constexpr int RV_3DMPPC_VERSION_MINOR = 0;
+#define RV_MPPC_VERSION_INFO_SECTION_NAME ".mppc_pdk_ver";
 
-inline constexpr const char* RV_ABI_INFO_SECTION = ".mppc_abi";
+inline constexpr int RV_MPPC_VERSION_MAJOR = 0;
+inline constexpr int RV_MPPC_VERSION_MINOR = 0;
 
-struct rv_mppc_abi_info {
-    char magic[8];  // MPPCABI\0 form marker
+// отвечает за то, а какое значение смотрим
+inline constexpr const char* RV_MPPC_VERSION_INFO_SECTION = RV_MPPC_VERSION_INFO_SECTION_NAME;
+
+// отвечает за обозначение секции внутри библиотеки для чтения, без исполнения кода
+struct rv_mppc_version_info {
+    char magic[8];  // marker {'R', 'V', '_', 'M', 'P', 'P', 'C', '\0'}
     uint32_t version_major;
     uint32_t version_minor;
 };
 
-static_assert(sizeof(rv_mppc_abi_info) == 16);
+// гарантирует побайтовую иднетичность для любой реализации
+static_assert(sizeof(rv_mppc_version_info) == 16);
 
-inline constexpr const char* RV_ABI_SYMBOL_CREATE = "mppc_disc_create";
-inline constexpr const char* RV_ABI_SYMBOL_DESTROY = "mppc_disc_destroy";
+inline constexpr const char* RV_MPPC_VERSION_SYMBOL_CREATE = "rv_mppc_disc_create";
+inline constexpr const char* RV_MPPC_VERSION_SYMBOL_DESTROY = "rv_mppc_disc_destroy";
 
-using rv_disc_create_fn = rv_de* (*)(int version_major, int version_minor);
-using rv_disc_destroy_fn = void (*)(rv_de* disc);
+using rv_mppc_disc_create_fn = rv_de* (*)(int32_t version_major, int32_t version_minor);
+using rv_mppc_disc_destroy_fn = void (*)(rv_de* disc);
 
 }  // namespace rv_pdk
