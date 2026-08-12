7c26967875ae86437c4af87bbcb7d537f7805719
Andrey Vydra <69751912+vydramain@users.noreply.github.com>
2026-08-12 20:57:39 +0600

    wip: Implementing LuaJIT

---
+++ mppcdiscs/example-lua/src/example-lua.cpp
@@ -0,0 +1,41 @@
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
+#include "pdk/de/rv_de.hpp"
+#include "pdk/ve/rv_ve.hpp"
+
+namespace example_lua
+{
+
+namespace
+{
+
+struct rv_example_lua_texheader {
+	uint16_t version;
+};
+
+} // namespace
+
+class rv_dmain : public rv_pdk::rv_de
+{
+public:
+	int64_t disc_initialize(rv_pdk::rv_pdko &pdk) override;
+	void frame_update(float dt) override;
+	void frame_render() override;
+	bool disc_release() const override
+	{
+		return release_;
+	}
+	void disc_shutdown() override;
+	const char *disc_tilte() const
+	{
+		return "example-lua";
+	};
+
+private:
+	rv_pdk::rv_pdko *pdk_ = nullptr;
+	bool release_ = false;
+};
+
+} // namespace example_lua
